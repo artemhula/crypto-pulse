c82550ec43858d9faf67b5c4b19042e3f4a223f5
Artem Hula <artemgul4@gmail.com>
2026-06-21 21:18:39 +0300

    fix: add missing redis module

--- apps/cron-parser/src/app/app.module.ts
+++ apps/cron-parser/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { RedisModule } from '@crypto-pulse/redis';
 import { RabbitExchange } from '@crypto-pulse/rabbitmq-common';
 import { CoinModule } from '@crypto-pulse/coin';
 import { CronParserService } from './app.service';
@@ -13,6 +14,7 @@ import { CronParserService } from './app.service';
     }),
     ScheduleModule.forRoot(),
     CoinModule,
+    RedisModule,
     RabbitMQModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
