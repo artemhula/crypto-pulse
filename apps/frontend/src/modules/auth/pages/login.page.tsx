6b2a4b4ad5ace10a5f60f598bd960d86b43293af
Artem Hula <artemgul4@gmail.com>
2026-09-16 22:51:28 +0300

    ref: change frontend app and update dependencies

--- apps/frontend/src/modules/auth/pages/login.page.tsx
+++
@@ -1,6 +0,0 @@
-import { AuthenticationForm } from '../components/authentication-form.component';
-import styles from './login.module.scss';
-
-export const LoginPage = () => {
-  return <AuthenticationForm className={styles.form} />;
-};
