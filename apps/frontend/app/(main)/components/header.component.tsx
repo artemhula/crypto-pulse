1e5cf57293fb4e7da6b5d7b298a333d90dc7b2c6
Artem Hula <artemgul4@gmail.com>
2026-09-18 19:49:38 +0300

    feat: implement user context and header component with authentication features

---
+++ apps/frontend/app/(main)/components/header.component.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useUser } from '@/context/user.context';
+import { logout } from '@/lib/auth/logout';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export const Header = () => {
+  const router = useRouter();
+  const { user, setUser } = useUser();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      setUser(null);
+      router.push('/');
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  };
+
+  return (
+    <header
+      className="flex items-center justify-between p-5 bg-gray-50
+"
+    >
+      <h1 className="text-xl font-bold">pulsee.</h1>
+      {user && (
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            render={
+              <Button variant="ghost" size="icon" className="rounded-full">
+                <Avatar>
+                  <AvatarImage src={user.avatarUrl} alt="shadcn" />
+                  <AvatarFallback>
+                    {user.name?.charAt(0).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+              </Button>
+            }
+          />
+          <DropdownMenuContent className="w-32">
+            <DropdownMenuGroup>
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem variant="destructive" onClick={handleLogout}>
+                Log out
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
+    </header>
+  );
+};
--- apps/frontend/app/(main)/layout.tsx
+++ apps/frontend/app/(main)/layout.tsx
@@ -1,10 +1,22 @@
+import { UserProvider } from '@/context';
+import { getCurrentUser } from '@/lib/auth/get-current-user';
 import { Header } from './components';
 
-export default function AppLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await getCurrentUser();
+
   return (
-    <>
-      <Header />
-      {children}
-    </>
+    <html lang="ru">
+      <body>
+        <UserProvider initialUser={user}>
+          <Header />
+          <main>{children}</main>
+        </UserProvider>
+      </body>
+    </html>
   );
 }
