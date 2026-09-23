555e43915de0bd6d205c8b5ee1e17eb2f13c4b95
Artem Hula <artemgul4@gmail.com>
2026-09-23 14:32:54 +0300

    feat: update coin type in frontend

--- apps/frontend/types/coin.type.ts
+++ apps/frontend/types/coin.type.ts
@@ -3,6 +3,9 @@ export type Coin = {
   symbol: string;
   name: string;
   currentPrice: number;
+  marketCap: number;
+  priceChangePercentage1h: number;
+  priceChangePercentage24h: number;
   image?: string;
   updatedAt: Date;
 };
